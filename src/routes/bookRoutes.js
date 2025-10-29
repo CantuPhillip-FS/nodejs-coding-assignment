ad2631bf9534c35d187bc6f5f37275c14e132cd5
hereisphil <thereisphil@gmail.com>
2025-10-29 07:45:09 -0600

    Added books to array, updated count variable, updated GET book by ID route message.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
-// localhost:5000/api/v1
 
-const books = [];
-let count = 1;
+const books = [
+  {
+    id: 1,
+    title: "Ender's Game",
+    author: "Orson Scott Card",
+  },
+  {
+    id: 2,
+    title: "Dune",
+    author: "Frank Herbert",
+  },
+  {
+    id: 3,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+  },
+  {
+    id: 4,
+    title: "1984",
+    author: "George Orwell",
+  },
+  {
+    id: 5,
+    title: "The Martian",
+    author: "Andy Weir",
+  },
+];
+let count = books[books.length - 1].id + 1;
 
 /* -------------------------------------------------------------------------- */
 /*                                     GET                                    */
@@ -17,12 +42,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  console.log(typeof req.params.id);
   const ID = parseInt(req.params.id);
   const book = books.find((book) => book.id === ID);
-  if (book === undefined) {
+  if (book === undefined || ID === NaN) {
     res.status(404).json({
-      message: `Book with ID: ${ID} cannot be found`,
+      message: `Book cannot be found. Please check the id.`,
       success: false,
     });
   } else {
