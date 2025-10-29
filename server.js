984fe2098cd8613b2b3106bee85f3d17413c7181
hereisphil <thereisphil@gmail.com>
2025-10-29 07:36:06 -0600

    Resolved the 403 error I was getting. Port 5000 was blocked and used by my Mac's AirPlay service. Changed PORT to 5001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
 const app = require("./src");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
